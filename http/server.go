7c7c3f6750ef8b80a7a67abdc915a03a74c9628a
ImGabreuw <imgabreuw.dev@gmail.com>
2021-07-06 18:58:41 -0300

    Webserver com Echo e Golang

---
+++ http/server.go
@@ -0,0 +1,39 @@
+package http
+
+import (
+	"github.com/Aprendendo-programacao/full-cycle-webserver-golang/model"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+type WebServer struct {
+	Products *model.Products
+}
+
+func NewWebServer() *WebServer {
+	return &WebServer{}
+}
+
+func (w WebServer) Serve() {
+	e := echo.New()
+
+	e.GET("/products", w.getAll)
+	e.POST("/products", w.createProduct)
+	e.Start(":8080")
+}
+
+func (w WebServer) getAll(ctx echo.Context) error {
+	return ctx.JSON(http.StatusOK, w.Products)
+}
+
+func (w WebServer) createProduct(ctx echo.Context) error {
+	product := model.NewProduct()
+
+	if err := ctx.Bind(product); err != nil  {
+		return err
+	}
+
+	w.Products.Add(product)
+
+	return ctx.JSON(http.StatusCreated, product)
+}
